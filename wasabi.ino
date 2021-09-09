319ecab176e918b2f4d79ee7409ec56cc2a4aa72
Myazakky <myazakky@rhino.local>
2021-09-10 07:34:09 +0900

    水温を送る

--- wasabi.ino
+++ wasabi.ino
@@ -1,8 +1,12 @@
 #include <M5Stack.h>
 #include "WiFiConnection.h"
 #include "TempratureModule.h"
+#include "Ambient.h"
+#include "AmbientConfig.h"
 
 TempratureModule tempModule;
+WiFiClient client;
+Ambient ambient;
 
 void setup()
 {
@@ -11,12 +15,14 @@ void setup()
 
   connectWiFi();
   tempModule.initialize();
+  ambient.begin(channelId, writeKey, &client);
 }
 
 void loop()
 {
   float temp = tempModule.waterTemprature();
   displayTemprature(temp);
+  sendTemprature(temp);
 }
 
 void displayTemprature(float temp)
@@ -24,4 +30,10 @@ void displayTemprature(float temp)
   M5.Lcd.setCursor(0, 20);
   M5.Lcd.print("temp: ");
   M5.Lcd.println(temp);
+}
+
+void sendTemprature(float temp)
+{
+  ambient.set(1, temp);
+  ambient.send();
 }
